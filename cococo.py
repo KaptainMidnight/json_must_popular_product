dc0342e81a88a660ae5fedd79094ac546ab30df8
KaptainMidnight <35865450+KaptainMidnight@users.noreply.github.com>
2019-01-26 23:33:57 +0400

    Update cococo.py

--- cococo.py
+++ cococo.py
@@ -2,30 +2,30 @@ import json
 
 
 class Analyser():  # Create class for analyse data.json
-    json_data = []
+    json_data = []  # Create list for json data
 
-    def load_data(self, file_name):
-        with open(file_name) as data:
-            self.json_data = json.loads(data.read())
+    def load_data(self, file_name):  # Function for load data json
+        with open(file_name) as data:  # Open file data.json
+            self.json_data = json.loads(data.read())  # Read data.json
 
-    def analyse(self, baskets):
-        count_product = dict()
-        for elements in baskets:
-            for product in elements:
-                product_in_counter = count_product.get(product["name"])
+    def analyse(self, baskets):  # Analysis json data
+        count_product = dict()  # Count product in data.json
+        for elements in baskets:  # Plunk element in dat.json
+            for product in elements:  # Plunk product in data.json
+                product_in_counter = count_product.get(product["name"])  # Search product counter in data.json with key name 
                 if product_in_counter:
-                    product_in_counter += product["count"]
-                    count_product.update({product["name"]: product_in_counter})
+                    product_in_counter += product["count"]  # Increases variable product_in_counter
+                    count_product.update({product["name"]: product_in_counter})  # Update dictionary count_product
                 else:
-                    count_product.update({product["name"]: product["count"]})
-
+                    count_product.update({product["name"]: product["count"]})  # Update dictionary count_product
+        '''Count rating product'''
         sorted_count = sorted(count_product, key=count_product.get, reverse=True)
         for r in sorted_count:
             print(r, count_product[r])
 
 
-analyser = Analyser()
-
+analyser = Analyser()  # Initialize class Analuyser
+'''Load data.json'''
 analyser.load_data("data.json")
 baskets = analyser.json_data["baskets"]
 analyser.analyse(baskets)
